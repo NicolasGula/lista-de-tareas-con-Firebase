f54e58c186f3664fafc0858146d47c97ad883556
NicolasBonzini <nicolas.bonzini@cencosud.com>
2022-10-26 10:35:01 -0300

    Update y delete terminados

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,18 +1,45 @@
 import { ReactComponent as Check } from "../../assets/img/check.svg";
 import { ReactComponent as Delete } from "../../assets/img/delete.svg";
+import firebase from "../../utils/firebase";
+import "firebase/compat/firestore";
 import "./task.scss";
+
+const db = firebase.firestore(firebase);
 
 const Task = (props) => {
   const { task } = props;
 
+  const completeTask = () => {
+    db.collection("tareas")
+      .doc(task.id)
+      .update({
+        completed: !task.completed,
+      })
+      .then(() => {
+        console.log("Tarea actualizada");
+      });
+  };
+
+  const deleteTask = () => {
+    db.collection("tareas")
+      .doc(task.id)
+      .delete()
+      .then(() => {
+        console.log("Tarea borrada");
+      });
+  };
+
   return (
     <div className="task">
       <div>
-        <Check />
+        <Check
+          onClick={completeTask}
+          className={task.completed ? "completed" : ""}
+        />
       </div>
       <div>{task.name}</div>
       <div>
-        <Delete />
+        <Delete onClick={deleteTask} />
       </div>
     </div>
   );
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,12 +2,12 @@ import firebase from "firebase/compat/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDP8hNjms9JLdb92f06cKfRvRUy_ci4kfk",
-  authDomain: "tareas-2000.firebaseapp.com",
-  projectId: "tareas-2000",
-  storageBucket: "tareas-2000.appspot.com",
-  messagingSenderId: "513817002105",
-  appId: "1:513817002105:web:e74e547cfb433ec97014a8",
+  apiKey: "AIzaSyBrvY7DqwrFuMMmKNL5g681-BmEdY427ZM",
+  authDomain: "tasks-b81c8.firebaseapp.com",
+  projectId: "tasks-b81c8",
+  storageBucket: "tasks-b81c8.appspot.com",
+  messagingSenderId: "845805262507",
+  appId: "1:845805262507:web:28ff44ff977698214749ee",
 };
 
 export default firebase.initializeApp(firebaseConfig);
