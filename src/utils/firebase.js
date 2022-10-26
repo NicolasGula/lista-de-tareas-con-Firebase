697057ad7a3ae0cb6c2406dce5229fe8a1a42eee
NicolasBonzini <nicolas.bonzini@cencosud.com>
2022-10-25 21:11:40 -0300

    Configure firebase e instale react bootstrap y sass

---
+++ src/utils/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDP8hNjms9JLdb92f06cKfRvRUy_ci4kfk",
+  authDomain: "tareas-2000.firebaseapp.com",
+  projectId: "tareas-2000",
+  storageBucket: "tareas-2000.appspot.com",
+  messagingSenderId: "513817002105",
+  appId: "1:513817002105:web:e74e547cfb433ec97014a8",
+};
+
+export default firebase.initializeApp(firebaseConfig);
