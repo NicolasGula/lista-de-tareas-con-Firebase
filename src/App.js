d8f77c8747e51d140b36c7e6b22d9d28cf5ee347
NicolasBonzini <nicolas.bonzini@cencosud.com>
2022-10-26 01:16:29 -0300

    Get tareas from firebase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,59 @@
-import { Container } from "react-bootstrap";
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import AddTask from "./components/AddTask/index";
+import { map } from "lodash";
+import firebase from "./utils/firebase";
+import "firebase/compat/firestore";
+import "./app.scss";
+
+const db = firebase.firestore(firebase);
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    db.collection("tareas")
+      .orderBy("completed")
+      .get()
+      .then((response) => {
+        const arrayTasks = [];
+        map(response.docs, (task) => {
+          const data = task.data();
+          data.id = task.id;
+          arrayTasks.push(data);
+        });
+        setTasks(arrayTasks);
+      });
+  }, []);
+
   return (
-    <Container className="app">
+    <Container fluid className="app">
       <div className="title">
         <h1>Lista de tareas con Firebase</h1>
       </div>
+      <Row className="todo">
+        <Col
+          className="todo__title"
+          xs={{ span: 10, offset: 1 }}
+          md={{ span: 6, offset: 3 }}
+        >
+          <h2>Tareas</h2>
+        </Col>
+        <Col
+          className="todo__list"
+          xs={{ span: 10, offset: 1 }}
+          md={{ span: 6, offset: 3 }}
+        >
+          <p>Lista de tareas</p>
+        </Col>
+        <Col
+          className="todo__list"
+          xs={{ span: 10, offset: 1 }}
+          md={{ span: 6, offset: 3 }}
+        >
+          <AddTask />
+        </Col>
+      </Row>
     </Container>
   );
 }
---
+++ src/components/AddTask/AddTask.js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+import { isEmpty } from "lodash";
+import firebase from "../../utils/firebase";
+import "firebase/compat/firestore";
+import { ReactComponent as Send } from "../../assets/img/send.svg";
+import "./addTask.scss";
+
+const db = firebase.firestore(firebase);
+
+export default function AddTask() {
+  const [task, setTask] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (!isEmpty(task)) {
+      db.collection("tareas")
+        .add({
+          name: task,
+          completed: false,
+        })
+        .then(() => {
+          setTask("");
+          console.log("Tarea Creada");
+        });
+    }
+  };
+
+  return (
+    <Form onSubmit={onSubmit} className="add-task">
+      <input
+        type="text"
+        placeholder="Nueva tarea..."
+        onChange={(e) => setTask(e.target.value)}
+        value={task}
+      />
+      <Button type="submit">
+        <Send />
+      </Button>
+    </Form>
+  );
+}
